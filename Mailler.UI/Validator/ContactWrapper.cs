f9f88295ba5d18567cc22539b733976050f6a0c9
Çağlar Dursun <dursuncaglar@gmail.com>
2017-12-16 23:48:00 +0300

    Validation class generalization

--- Mailler.UI/Validator/ContactWrapper.cs
+++ Mailler.UI/Validator/ContactWrapper.cs
@@ -7,30 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace Mailler.UI.Validator
 {
-    public class ContactWrapper : NotifyDataErrorInfoBase
+    
+    public class ContactWrapper : ModelWrapper<Contact>
     {
-        public ContactWrapper(Contact model)
+        public ContactWrapper(Contact model):base(model)
         {
             Model = model;
         }
 
         public Contact Model { get; }
         public int Id { get{ return Model.Id; } }
-
-        public string Name
-        {
-            get {
-                return Model.Name;
-            } set {
-                Model.Name = value;
-                OnPropertyChanged();
-                ValidateProperty(nameof(Name));
-            }
-        }
-
+        
         private void ValidateProperty(string propertyName)
         {
             ClearErrors(propertyName);
@@ -46,31 +37,12 @@ namespace Mailler.UI.Validator
                     break;
             }
         }
+        
+        public string Name { get { return GetValue<string>(nameof(Name)); } set { SetValue(value); } }
 
-        public string Surname
-        {
-            get {
-                return Model.Surname;
-            }
-            set {
-                Model.Name = value;
-                OnPropertyChanged();
-            }
-        }
+        public string Surname{ get { return GetValue<string>(nameof(Name)); } set { SetValue(value); } }
 
-
-        public string EMail
-        {
-            get
-            {
-                return Model.EMail;
-            }
-            set
-            {
-                Model.EMail = value;
-                OnPropertyChanged();
-            }
-        }
+        public string EMail { get { return GetValue<string>(nameof(Name)); } set { SetValue(value);} }
 
        
 
---
+++ Mailler.UI/Validator/ModelWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mailler.UI.Validator
+{
+    public class ModelWrapper<T> : NotifyDataErrorInfoBase
+    {
+        public ModelWrapper(T model)
+        {
+            Model = model;
+        }
+
+        public T Model { get; }
+
+        protected virtual TValue GetValue<TValue>([CallerMemberName] string propertyName = null)
+        {
+            return (TValue)typeof(T).GetProperty(propertyName).GetValue(Model);
+        }
+
+        protected virtual void SetValue<TValue>(TValue value,
+            [CallerMemberName] string propertyName = null)
+        {
+            typeof(T).GetProperty(propertyName).SetValue(Model, value);
+            OnPropertyChanged(propertyName);
+        }
+    }
+}

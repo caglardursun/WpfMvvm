b78c591ae28dfaaa6f1cd5f5c2f9826948e35568
Çağlar Dursun <dursuncaglar@gmail.com>
2017-12-23 08:23:00 +0300

    Nativagion Style Added

--- Mailler.UI/ViewModel/NavigationItemViewModel.cs
+++ Mailler.UI/ViewModel/NavigationItemViewModel.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Mailler.UI.Event;
+using Prism.Commands;
+using Prism.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Mailler.UI.ViewModel
 {
@@ -10,17 +14,32 @@ namespace Mailler.UI.ViewModel
     {
         private string _displayMember;
 
-        public NavigationItemViewModel(int id,string displayMember)
+        public NavigationItemViewModel(int id,string displayMember,IEventAggregator eventAggregator)
         {
             Id = id;
             DisplayMember = displayMember;
+            _eventAggregator = eventAggregator;
+            OpenContactDetailViewCommand = new DelegateCommand(OnOpenContactDetailView);
         }
+
+        
 
         public int Id { get; }
         public string DisplayMember
         {
             get { return _displayMember; }
             set { _displayMember = value; OnPropertyChanged(); }
+        }
+
+        private IEventAggregator _eventAggregator;
+
+        public ICommand OpenContactDetailViewCommand { get; set; }
+
+        private void OnOpenContactDetailView()
+        {
+            _eventAggregator
+                .GetEvent<OpenContactDetailViewEvent>()
+                .Publish(Id);
         }
 
     }
--- Mailler.UI/ViewModel/NavigationViewModel.cs
+++ Mailler.UI/ViewModel/NavigationViewModel.cs
@@ -39,30 +39,12 @@ namespace Mailler.UI.ViewModel
             Contacts.Clear();
             foreach (var item in lookUp)
             {
-                Contacts.Add(new NavigationItemViewModel(item.Id, item.DisplayMember));
+                Contacts.Add(new NavigationItemViewModel(item.Id, item.DisplayMember,_eventAggregator));
             }
         }
 
 
         public ObservableCollection<NavigationItemViewModel> Contacts { get; }
-
-        private NavigationItemViewModel _selectedContact;
-
-        public NavigationItemViewModel SelectedContact
-        {
-            get { return _selectedContact; }
-            set
-            {
-                _selectedContact = value;
-                OnPropertyChanged();
-                if (_selectedContact != null)
-                {
-                    _eventAggregator
-                        .GetEvent<OpenContactDetailViewEvent>()
-                        .Publish(_selectedContact.Id);
-                }
-            }
-        }
 
 
     }
